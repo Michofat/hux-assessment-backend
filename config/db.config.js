90fdf660d7fe4e050db0aadd1f96b8493438ecd2
Michofat <michofatltd@gmail.com>
2024-05-05 03:37:39 +0100

    Added MySQL database configuration and implemented secure storage of sensitive keys in .env file

---
+++ config/db.config.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+export const dbConn = {
+  HOST: process.env.DBHOST,
+  USER: process.env.DBUSER,
+  PASSWORD: process.env.DBPASSWORD,
+  DB: process.env.DB,
+
+  dialect: "mysql",
+  pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+};
